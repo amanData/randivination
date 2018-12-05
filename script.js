0489ae4074937470e83455bc3248b70ef5afced6
Amanda Thouvenin <amanda.thouvenin@gmail.com>
2018-12-05 16:51:44 +0100

    better card layout

--- script.js
+++ script.js
@@ -34,15 +34,28 @@ function getRndInteger(min, max) {
 function drawCard() {
 	var i = getRndInteger(0,symbols.length-1);
 	var j = getRndInteger(0,cardNbs.length-1);
-	card.innerHTML = '<div class="cardNb">'+cardNbs[j]+'</div>';
-	if ((j>0)&&(j<10)) {
+	
+	if ((j>=0)&&(j<10)) {
+		card.innerHTML = '<div class="cardNbTopLeft">'+cardNbs[j]+'<br>'+symbols[i]+'</div>'+'<div class="cardNbBottomRight">'+cardNbs[j]+'<br>'+symbols[i]+'</div>';
 		for (var n = 0; n <= j; n++) {
-		card.innerHTML += symbols[i];
+		card.innerHTML += '<p class="symbol">'+symbols[i]+'</p>';
 		}
 	}
+	else if (j == cardNbs.length-1) {
+		card.innerHTML = '<div class="cardNbTopLeft"><i class="fas fa-star-of-life"></i></div>'+'<div class="cardNbBottomRight"><i class="fas fa-star-of-life"></i></div>';
+		card.innerHTML += '<i class="fas fa-user-secret"></i>';
+	}
 	else {
-		card.innerHTML += symbols[i];
+		card.innerHTML = '<div class="cardNb">'+cardNbs[j]+'</div>';
+		card.innerHTML += '<p class="symbol">'+symbols[i]+'</p>';
 	}
 }
 
+function onEnterOrSpace(event) {
+	if(event.keyCode == 32 || event.keyCode == 13) {
+        drawCard();
+ 	}
+}
+
 card.addEventListener('click', drawCard);
+document.addEventListener('keyup', onEnterOrSpace);
